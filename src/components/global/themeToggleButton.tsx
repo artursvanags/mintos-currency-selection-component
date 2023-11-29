1bab445d74a3bc77537c6a91de66731c22b17601
Arturs <arturs.vanags320@gmail.com>
2023-11-29 19:14:26 +0200

    Add theme provider and theme toggle button components

---
+++ src/components/global/themeToggleButton.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import * as React from 'react';
+import { useTheme } from 'next-themes';
+
+import { ThemeIcons } from '@/config/icons';
+
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { cn } from '@/lib/utils';
+
+
+export default function ModeToggle({...props}: React.HTMLAttributes<HTMLButtonElement>) {
+  const { setTheme } = useTheme();
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button className={cn(props.className)} variant="outline" size="icon">
+          <ThemeIcons.Sun className="h-4 w-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <ThemeIcons.Moon className="absolute h-4 w-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme('light')}>
+          <ThemeIcons.Sun className="mr-2 h-4 w-4" /> Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme('dark')}>
+          <ThemeIcons.Moon className="mr-2 h-4 w-4" /> Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme('system')}>
+          <ThemeIcons.System className="mr-2 h-4 w-4" /> System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
