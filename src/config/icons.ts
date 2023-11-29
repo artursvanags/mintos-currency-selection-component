c613f6a359b77ab5c8a15352659de90b7519cb99
Arturs <arturs.vanags320@gmail.com>
2023-11-29 19:14:34 +0200

    Add icons configuration file

---
+++ src/config/icons.ts
@@ -0,0 +1,13 @@
+import { X, BoxSelect, ChevronDown, Sun, Moon, Laptop } from 'lucide-react';
+
+export const Icons = {
+  clear: X,
+  select: BoxSelect,
+  dropdown: ChevronDown,
+};
+
+export const ThemeIcons = {
+  Sun: Sun,
+  Moon: Moon,
+  System: Laptop,
+};
