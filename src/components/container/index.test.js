eea8c607399eaecd34fbdcb5ffcfd1916c0551f0
Arturs <arturs.vanags320@gmail.com>
2023-11-30 16:01:21 +0200

    Add test scripts and dependencies for Jest

---
+++ src/components/container/index.test.js
@@ -0,0 +1,83 @@
+/**
+ * @jest-environment jsdom
+ */
+import React from 'react';
+import { render, fireEvent, screen, within } from '@testing-library/react';
+import SelectionContainer from './index';
+
+const data = [{ code: 'USD' }, { code: 'EUR' }];
+
+describe('SelectionContainer', () => {
+  let container;
+
+  beforeEach(() => {
+    const { container: renderedContainer } = render(
+      <SelectionContainer data={data} />,
+    );
+    container = renderedContainer;
+  });
+
+  it('Renders without crashing.', () => {
+    expect(container).toBeInTheDocument();
+  });
+
+  it('Displays the correct number of items.', () => {
+    const itemList = screen.getByLabelText('item-list');
+    const items = within(itemList).getAllByRole('button');
+    expect(items).toHaveLength(data.length);
+  });
+
+  it('Filters items based on search input.', () => {
+    const searchInput = screen.getByLabelText('search-input');
+    fireEvent.change(searchInput, { target: { value: 'us' } });
+    expect(screen.queryByText('USD')).toBeInTheDocument();
+    expect(screen.queryByText('EUR')).not.toBeInTheDocument();
+  });
+
+  it('Toggles items correctly.', () => {
+    const usdButton = screen.getByText('USD');
+    expect(usdButton).toHaveAttribute('aria-pressed', 'false');
+
+    fireEvent.click(usdButton);
+    expect(usdButton).toHaveAttribute('aria-pressed', 'true');
+
+    fireEvent.click(usdButton);
+    expect(usdButton).toHaveAttribute('aria-pressed', 'false');
+  });
+
+  it('Removes individual items correctly.', () => {
+    const usdButton = screen.getByText('USD');
+    fireEvent.click(usdButton);
+
+    const selectedItemList = screen.getByLabelText('selected-items-list');
+    const selectedItem = within(selectedItemList).getByText('USD');
+    expect(selectedItem).toBeInTheDocument();
+
+    const removeItemIcon = within(selectedItem).getByLabelText('remove-item');
+    fireEvent.click(removeItemIcon);
+
+    expect(within(selectedItemList).queryByText('USD')).not.toBeInTheDocument();
+  });
+
+  it('Selects all items correctly.', () => {
+    const selectAllButton = screen.getByLabelText('select-all');
+    fireEvent.click(selectAllButton);
+
+    const selectedItemList = screen.getByLabelText('selected-items-list');
+    const items = within(selectedItemList).getAllByRole('button');
+    expect(items).toHaveLength(data.length);
+  });
+
+  it('Clears all items correctly.', () => {
+    const usdButton = screen.getByText('USD');
+    fireEvent.click(usdButton);
+    const eurButton = screen.getByText('EUR');
+    fireEvent.click(eurButton);
+
+    const clearButton = screen.getByLabelText('clear-all');
+    fireEvent.click(clearButton);
+
+    const selectedItemList = screen.queryByLabelText('selected-items-list');
+    expect(selectedItemList).not.toBeInTheDocument();
+  });
+});
